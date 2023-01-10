030d2683f14e40356e981a5234097c64627afdb2
UitsTanVirH <uits.tanvirh@gmail.com>
2023-01-10 22:09:47 +0600

    first commit

---
+++ src/components/navbar.component.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+	state = {};
+	render() {
+		return (
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<div className="container-fluid">
+					<a className="navbar-brand">
+						Youtube
+					</a>
+				</div>
+			</nav>
+		);
+	}
+}
+
+export default Navbar;
